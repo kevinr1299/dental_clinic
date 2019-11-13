bf4d32fe11e0573a5dcd247eb0550fe26bb5e9e4
Kevin Romero <kevin.romero@red4g.net>
2019-11-12 18:10:47 -0600

    seed

---
+++ database/seeds/RolesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Role;
+use Illuminate\Database\Seeder;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new Role();
+        $role->name = 'Administrador';
+        $role->save();
+
+        $role = new Role();
+        $role->name = 'Secretaria';
+        $role->save();
+    }
+}
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'admin';
+        $user->password = Hash::make('123456');
+        $user->role_id = 1;
+        $user->save();
+    }
+}
